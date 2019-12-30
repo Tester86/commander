0aa92f991d2f1dc6dc646e12982a3bd7047e4599
Tester86 <tester86t@gmail.com>
2019-12-30 23:33:52 +0100

    Adding additional features

--- src/Quick_shell.cpp
+++ src/Quick_shell.cpp
@@ -20,12 +20,17 @@ string cmd_mob;
 vector<string> cmd;
 
 while(1){
-	cout << getdir() << " > " ;
+	cout << getdir() << "> " ;
 	getline(cin, cmd_mob);
 	cmd = split_string(cmd_mob, " ");
 	if(cmd[0] == "help"){
-		help("a");
+		if(cmd_mob == "help"){
+			help("a");
+		}
+		else{
+			help(cmd[1]);
 	}
+}
 	 else if(cmd[0] == "write"){
 	 	init_fw(cmd[1], "w");
 	 }
@@ -98,22 +103,22 @@ void help(const string elem){
 	if(elem == "miscelanea" || elem == "general" || elem == "overall"){
 	cout << "\nTask:                                Function:" << endl << endl;
 	cout << "  help                                  Shows help menu" << endl;
-	cout << "  clear / cls                           Clear screen" << endl;
-	cout << "  music                                 Opens music menu" << endl << endl << endl;
-} else if(elem == "file"){
-	cout << "Files:" << endl;
+	cout << "  clear / cls                           Clear screen" << endl << endl;
+} else if(elem == "files"){
+	cout << "\nFiles:" << endl;
 	cout << "  write [file]                                 Opens code editor to write a file" << endl;
 	cout << "  append [file]                                Opens code editor to append to a file" << endl;
 	cout << "  read [file]                                  Prints content of a file onscreen" << endl;
 	cout << "  copy [file] to [new file path]               Copies the content of a file to another file" << endl;
-	cout << "  remove / rm [file / dir]                           Removes a file" << endl;
+	cout << "  remove / rm [file / dir]                     Removes a file" << endl << endl;
 } else if(elem == "music"){
+	cout << "\n  music                                           Opens music menu" << endl;
 	cout << "  play [song filename]                            Plays a song in predetermined music player" << endl;
-	cout << "  *download / dw [youtube or google link]         Downloads .mp3 file to Music folder" << endl;
+	cout << "  *download / dw [youtube or google link]         Downloads .mp3 file to Music folder" << endl << endl;
 } else{
 	cout << "\nUsage of help:" << endl;
 	cout << "   Help [element]";
 	cout << "\n\nElements available: \n" << endl;
-	cout << "file" << endl << "music" << endl << "overall / miscelanea / general" << endl << endl;
+	cout << "files" << endl << "music" << endl << "overall / miscelanea / general" << endl << endl;
 }
 }
